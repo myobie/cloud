98bc1b64ed88b60453e1a5f354509cb021858d74
myobie <nathan@myobie.com>
2013-04-18 15:27:06 -0700

    Detabbed the files, not sure how those tabs got in there

--- kite.boxes.rb
+++ kite.boxes.rb
@@ -1,53 +1,53 @@
 class LoadBalancer < Box
-	memory "512MB"
-	deps 'myobie.nginx', 'myobie.nginx.assets', 'myobie.haproxy', 'myobie.rails'
-	parse_opts :balance
-	config { Kite.config['load_balancer'] }
+  memory "512MB"
+  deps 'myobie.nginx', 'myobie.nginx.assets', 'myobie.haproxy', 'myobie.rails'
+  parse_opts :balance
+  config { Kite.config['load_balancer'] }
 end
 
 class Application < Box
-	memory "1GB"
+  memory "1GB"
   deps 'myobie.nginx', 'myobie.nginx.unicorn', 'myobie.rails',
        'myobie.rails.git', 'myobie.rails.unicorn'
-	parse_opts :git_url
-	config { Kite.config['application'] }
+  parse_opts :git_url
+  config { Kite.config['application'] }
 end
 
 class Database < Box
-	memory "1GB"
-	deps 'myobie.postgres'
+  memory "1GB"
+  deps 'myobie.postgres'
 end
 
 class SlaveDatabase < Database
-	parse_opts :follow, :failover
+  parse_opts :follow, :failover
 end
 
 class Redis < Box
-	memory "1GB"
-	deps 'myobie.redis'
-	config { Kite.config['redis'] }
-	
-	def config_file
-	  @databases = 16
-	  render text: <<-EOF
+  memory "1GB"
+  deps 'myobie.redis'
+  config { Kite.config['redis'] }
+  
+  def config_file
+    @databases = 16
+    render text: <<-EOF
 some file things
 databases <%= @databases %>
 more file things
-	  EOF
-	end
+    EOF
+  end
 end
 
 class SlaveRedis < Redis
-	parse_opts :follow, :failover
-	config { Kite.config['slave_redis'] }
+  parse_opts :follow, :failover
+  config { Kite.config['slave_redis'] }
 end
 
 class Memcached < Box
-	memory "1GB"
-	deps 'myobie.memcached'
+  memory "1GB"
+  deps 'myobie.memcached'
 end
 
 class Resque < Box
-	memory "1GB"
-	deps 'myobie.resque'
+  memory "1GB"
+  deps 'myobie.resque'
 end
